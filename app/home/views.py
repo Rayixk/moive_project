1934650247683f0cf7dd867ee0f0e7a64d048cff
jackChen8850 <472093743@qq.com>
2017-11-09 23:08:30 +0800

    user:登录&注册

--- app/home/views.py
+++ app/home/views.py
@@ -2,43 +2,102 @@
 # Created by yang
 
 from . import home
-from flask import render_template,redirect,url_for
+from flask import render_template, redirect, url_for, flash, session, request, Response
+from app.home.forms import RegistForm,LoginForm
+from app.models import User, Userlog, Preview, Tag, Movie, Comment, Moviecol
+from werkzeug.security import generate_password_hash
+from werkzeug.utils import secure_filename
+from app import db, app
+from functools import wraps
+import uuid
+import os
+import datetime
+
+# 登录装饰器
+def user_login_req(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "user" not in session:
+            return redirect(url_for("home.login", next=request.url))
+        return f(*args, **kwargs)
+
+    return decorated_function
 
 
-
-@home.route("/login/")
+@home.route("/login/", methods=["GET", "POST"])
 def login():
-    return render_template("home/login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User.query.filter_by(name=data["name"]).first()
+        if not user:
+            flash("账号不存在！", "err")
+            return redirect(url_for("home.login"))
+        if not user.check_pwd(data["pwd"]):
+            flash("密码错误！", "err")
+            return redirect(url_for("home.login"))
+        session["user"] = user.name
+        session["user_id"] = user.id
+        userlog = Userlog(
+            user_id=user.id,
+            ip=request.remote_addr
+        )
+        db.session.add(userlog)
+        db.session.commit()
+        return redirect(request.args.get("next") or url_for("home.user"))
+    return render_template("home/login.html", form=form)
 
 @home.route("/logout/")
 def logout():
+    session.pop("user", None)
+    session.pop("user_id", None)
     return redirect(url_for("home.login"))
 
-@home.route("/regist/")
+# 会员注册
+@home.route("/regist/", methods=["GET", "POST"])
 def regist():
-    return render_template("home/regist.html")
+    form = RegistForm()
+    if form.validate_on_submit():
+        data = form.data
+        user = User(
+            name=data["name"],
+            email=data["email"],
+            phone=data["phone"],
+            pwd=generate_password_hash(data["pwd"]),
+            uuid=uuid.uuid4().hex
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("注册成功！", "ok")
+    return render_template("home/regist.html", form=form)
 
 @home.route("/user/")
+@user_login_req
 def user():
     return render_template("home/user.html")
 
 @home.route("/pwd/")
+@user_login_req
 def pwd():
     return render_template("home/pwd.html")
 
 @home.route("/comments/")
+@user_login_req
 def comments():
     return render_template("home/comments.html")
 
 @home.route("/loginlog/")
+@user_login_req
 def loginlog():
     return render_template("home/loginlog.html")
 
 @home.route("/moviecol/")
+@user_login_req
 def moviecol():
     return render_template("home/moviecol.html")
 
 @home.route("/")
+
 def index():
     return render_template("home/index.html")
 
